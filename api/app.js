618ff5c93597a65f790279926c1a7372a166896a
Gabriel Dumortier <gabrieldumortier@MacBook-Pro-de-Gabriel.local>
2019-10-17 11:18:53 +0200

    start app.js + install body-parser

--- api/app.js
+++ api/app.js
@@ -1,3 +1,23 @@
 import express from 'express';
-
 import {connect} from './utils/db';
+import {json, urlencoded} from 'body-parser';
+
+
+// const app = express();
+
+// app.use(json());
+// app.use(urlencoded({extended:true}));
+
+// app.use('/api', dbRouter); // dbRouter a importer
+// const start = async () => {
+//     try {
+//         await connect();
+//         app.listen(8001, () => {
+//             console.log('Rest api is listening on port 8001');
+//         });
+//     } catch(err) {
+//         throw err;
+//     }
+// }
+
+// start();

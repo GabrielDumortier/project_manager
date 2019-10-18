b48e0d9f009a411d6c2a90a62985f203e73224f2
RomainHaas <romainhaas@pop-os.localdomain>
2019-10-18 13:55:54 +0200

    projectModel

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -1,2 +1,150 @@
 const colorValidator = (v) => (/^#([0-9a-f]{3}){1,2}$/i).test(v);
 
+export class Project {
+    constructor (
+        public name? : string,
+        public description? : string,
+        public author_id? : string,
+        public creation_date? : number,
+        public start_date? : number,
+        public finish_date? : number,
+        public deadline? : number,
+        public status? : string,
+        public users? : {
+            user_id? : string,
+            function? : string,
+            role? : string
+        },
+        public is_private? : boolean,
+        public attachements? : [
+            {
+                path? : string
+            },
+            {
+                name? : string
+            },
+            {
+                description? : string
+            }
+        ],
+        public comments? : [
+            {
+                author_id? : string
+            },
+            {
+                comment? : string
+            },
+            {
+                date? : number
+            }
+        ],
+        public git? : string,
+        public ressources? : [
+            {
+                name? : string
+            },
+            {
+                description? : string
+            },
+            {
+                url? : string
+            },
+            {
+                author? : string
+            },
+            {
+                date? : number
+            },
+        ],
+        public tasks? : [
+            {
+                name? : string
+            },
+            {
+                description? : string
+            },
+            {
+                author_id? : string
+            },
+            {
+                labels? : [
+                    {
+                        name? : string
+                    },
+                    {
+                        color? : string
+                    },
+                ],
+            },
+            {
+                assigned? : [
+                    {
+                        user_id? : string
+                    },
+                    {
+                        spend? : number
+                    },
+                ]
+            },
+            {
+                checklist? : [
+                    {
+                        name? : string
+                    },
+                    {
+                        done? : boolean
+                    },
+                ]
+            },
+            {
+                deadline? : number
+            },
+            {
+                progression? : number
+            },
+            {
+                estimated? : number
+            },
+            {
+                priority? : string
+            },
+            {
+                attachments? : [
+                    {
+                        name? : string
+                    },
+                    {
+                        description? : string
+                    },
+                    {
+                        path? : string
+                    },
+                    {
+                        author_id? : string
+                    },
+                    {
+                        date? : number
+                    }
+                ]
+            },
+            {
+                comments? : [
+                    {
+                        author_id? : string
+                    },
+                    {
+                        comment? : string
+                    },
+                    {
+                        date? : number
+                    }
+                ]
+            },
+                {
+                    status? : string
+                }
+        ]
+        
+    ) {}
+
+}
